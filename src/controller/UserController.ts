47c1c709a1852d4d8100e8dfa1ef77acf7ab97a5
Fatma Kaya <fatma.kaya@arneca.com>
2022-03-09 15:49:15 +0300

    added validation to users.

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -7,35 +7,51 @@ export class UserController {
     private userBookRepository = getRepository(UserBook);
 
     async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find();
+        await this.userRepository.find().then((users)=>{
+            response.status(200).send(users);
+        }).catch((error)=>{
+            next(error);
+        });
     }
 
     async one(request: Request, response: Response, next: NextFunction) {
-        let user = await this.userRepository.findOne(request.params.id)
+        await this.userRepository.findOne(request.params.id).then(async (user) => {
+            if (user) {
+                const userbooks = await this.userBookRepository.find({
+                    relations: ['user', 'book'],
+                    where: { user: { id: request.params.id } },
+                });
 
-        const userbooks = await this.userBookRepository.find({
-            relations: ['user', 'book'],
-            where: { user: { id: request.params.id } },
+                let present = []
+                let past = []
+
+                userbooks.map(ub => {
+                    ub.score === null ? present.push(ub.book) : past.push({ ...ub.book, score: ub.score })
+                })
+
+                response.status(200).send({
+                    ...user,
+                    books: {
+                        past: past,
+                        present: present,
+                    }
+                });
+            }
+            else
+                response.status(404).send({ error: 'User not found' });
+
+        }).catch((error) => {
+            next(error);
         });
 
-        let present = []
-        let past = []
-
-        userbooks.map(ub => {
-            ub.score === null ? present.push(ub.book) : past.push({ ...ub.book, score: ub.score })
-        })
-
-        return {
-            ...user,
-            books: {
-                past: past,
-                present: present,
-            }
-        }
     }
 
     async save(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.save(request.body);
+        await this.userRepository.save(request.body).then((users)=>{
+            response.status(200).send(users);
+        }).catch((error)=>{
+            next(error);
+        });
     }
 
     async remove(request: Request, response: Response, next: NextFunction) {
